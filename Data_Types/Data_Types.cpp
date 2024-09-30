c6a7b43eaf01e936deeb855da98126410ac280b1
Jheferson Añonuevo <jhefersonzanonuevo@gmail.com>
2024-10-01 02:09:56 +0800

    add variables with different data types

--- Data_Types/Data_Types.cpp
+++ Data_Types/Data_Types.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main() {
 	int number1 = 2;
+	float number2 = 2.91929F;
+	double number3 = 3.21312312312313;
+	char letter = 'a';
+	bool confession = true;
 
-	cout << "This is an Integer " << number1;
-
+	cout << "This is an Integer " << number1 << "\n";
+	cout << "This is a Float " << number2 << "\n";
+	cout << "This is a Double " << number3 << "\n";
+	cout << "This is a Char " << letter << "\n";
+	cout << "this is a Boolean " << confession << "\n";
 	system("pause>0");
 }
